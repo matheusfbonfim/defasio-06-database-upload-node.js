4620d9b958c9810423c87a2c1a3d28b47f35f25a
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2021-02-04 00:38:02 -0300

    up

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
@@ -7,20 +7,28 @@ import { Router } from 'express';
 
 const transactionsRouter = Router();
 
-transactionsRouter.get('/', async (request, response) => {
+transactionsRouter.get('/', async (request: Request, response: Response) => {
   // TODO
 });
 
-transactionsRouter.post('/', async (request, response) => {
-  // TODO
+transactionsRouter.post('/', async (request: Request, response: Response) => {
+  const { id, title, value, type, category } = request.body;
+  
+
 });
 
-transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
-});
+transactionsRouter.delete(
+  '/:id',
+  async (request: Request, response: Response) => {
+    // TODO
+  },
+);
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  async (request: Request, response: Response) => {
+    // TODO
+  },
+);
 
 export default transactionsRouter;
